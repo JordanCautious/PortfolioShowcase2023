3d9be5fb2a9d78b93a9aeed81c4f0c0f2df0b018
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-10-07 21:20:25 -0400

    Initial Commit

---
+++ PortfolioShowcase2023/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PortfolioShowcase2023
+//
+//  Created by Jordan Haynes on 10/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PortfolioShowcase2023/PortfolioShowcase2023App.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioShowcase2023App.swift
+//  PortfolioShowcase2023
+//
+//  Created by Jordan Haynes on 10/7/23.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioShowcase2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
